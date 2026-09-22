0f40a725b52e918edf1ecd1bd0e1d7f316e6a807
Navnath-Msquare <navnath.b@msquaresoftware.com>
2026-09-22 16:34:10 +0530

    fix(routes): uncomment payment and reconciliation routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -45,7 +45,7 @@ router.use("/sos", require("../../modules/sos/sos.routes"));
 // router.use("/societies", societyRoutes);
 router.use("/billing", require("../../modules/billing/billing.routes"));
 router.use("/documents", require("../../modules/document/document.routes"));
-// router.use("/payments", require("../../modules/payment/payment.routes"));
-// router.use("/reconciliation", require("../../modules/reconciliation/reconciliation.routes"));
+router.use("/payments", require("../../modules/payment/payment.routes"));
+router.use("/reconciliation", require("../../modules/reconciliation/reconciliation.routes"));
 
 module.exports = router;
